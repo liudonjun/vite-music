575e856fda0389e3f7779fadb0eba1e9573b678f
Coder <3170495286@qq.com>
2023-02-06 17:03:27 +0800

    feat:development config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  esbuild: {
+    // development
+    pure: true ? ['console.log', 'debugger'] : [],
+  },
+  build: {
+    target: 'es2015',
+    cssTarget: 'chrome80',
+    outDir: 'dist',
+    chunkSizeWarningLimit: 2000,
+  },
 })
