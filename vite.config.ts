46e793d12ae6352c950bbaedc0c9df87297e3c5c
Coder <3170495286@qq.com>
2023-02-09 17:31:11 +0800

    fix:update types

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,40 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   esbuild: {
     // development
-    pure: true ? ['console.log', 'debugger'] : [],
+    pure: true ? ['console.log', 'debugger'] : []
+  },
+  resolve: {
+    alias: [
+      {
+        find: 'vue-i18n',
+        replacement: 'vue-i18n/dist/vue-i18n.cjs.js'
+      },
+      // /@/xxxx => src/xxxx
+      {
+        find: /\/@\//,
+        replacement: pathResolve('src') + '/'
+      },
+      // /#/xxxx => types/xxxx
+      {
+        find: /\/#\//,
+        replacement: pathResolve('types') + '/'
+      }
+    ]
   },
   build: {
     target: 'es2015',
     cssTarget: 'chrome80',
     outDir: 'dist',
-    chunkSizeWarningLimit: 2000,
-  },
+    chunkSizeWarningLimit: 2000
+  }
 })
